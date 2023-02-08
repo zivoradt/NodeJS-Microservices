35cc789fd347cf10d48b4dec80fe7736fdfa5a7c
zivoradt <zivoradtrickovic@gmail.com>
2023-02-08 19:58:26 +0100

    added

---
+++ shopping/src/config/index.js
@@ -0,0 +1,18 @@
+const dotenv  = require("dotenv");
+
+if( process.env.NODE_ENV !== 'prod')
+{
+    const configFile = "./.env.dev";
+    dotenv.config({path: configFile});
+}
+else
+{
+    dotenv.config();
+   
+}
+
+module.exports = {
+    PORT: process.env.PORT,
+    DB_URL: process.env.MONGODB_URI,
+    APP_SECRET: process.env.APP_SECRET
+}
