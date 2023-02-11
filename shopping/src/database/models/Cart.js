68298696cc32ffd85c0eff3407fd61ecc996a30d
zivoradt <zivoradtrickovic@gmail.com>
2023-02-11 10:27:27 +0100

    customisiing shopping microservice

--- shopping/src/database/models/Cart.js
+++ shopping/src/database/models/Cart.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema({
-    orderId: String,
-    customerId: String,
-    amount: Number,
-    status: String,
-    txnId: String,
+const CartSchema = new Schema({
+    
+    customerId: {type: String},
     items: [
         {
             product: {
@@ -23,6 +20,7 @@ const OrderSchema = new Schema({
               unit: {type: Number, require: true}
         }
     ]
+
 },
 {
     toJSON: {
@@ -33,4 +31,4 @@ const OrderSchema = new Schema({
     timestamps: true
 });
 
-module.exports =  mongoose.model('order', OrderSchema);
+module.exports = mongoose.model('card', CartSchema);
--- shopping/src/database/repository/shopping-repository.js
+++ shopping/src/database/repository/shopping-repository.js
@@ -1,65 +1,134 @@
-const mongoose = require('mongoose');
-const { CustomerModel, ProductModel, OrderModel } = require('../models');
-const { v4: uuidv4 } = require('uuid');
+const mongoose = require("mongoose");
+const {OrderModel, CartModel,} = require("../models");
+const { v4: uuidv4 } = require("uuid");
 
 //Dealing with data base operations
 class ShoppingRepository {
+  // payment
 
-    // payment
-
-    async Orders(customerId){
-        
-        const orders = await OrderModel.find({customerId }).populate('items.product');        
-        return orders;
+  async Orders(customerId) {
+    try {
+      const orders = await OrderModel.find({ customerId });
+      return orders;
+    } catch {
+      throw APIError(
+        "API Error",
+        STATUS_CODES.INTERNAL_ERROR,
+        "Unable to find orders"
+      );
     }
- 
- 
-    async CreateNewOrder(customerId, txnId){
+  }
 
-        //check transaction for payment Status
+  async Cart(customerId) {
+    try {
+      const cartItems = await CartModel.find({ customerId: customerId });
 
-        
-        const profile = await CustomerModel.findById(customerId).populate('cart.product');
+      if (cartItems) {
+        return cartItems;
+      }
+      throw new Error("Data not found");
+    } catch (err) {
+      throw err;
+    }
+  }
 
-        if(profile){
-            
-            let amount = 0;   
+  async AddCartItem(customerId, item, qty, isRemove) {
+    try {
+      const cart = await CartModel.findOne({ customerId: customerId });
 
-            let cartItems = profile.cart;
+      const { _id } = item;
 
-            if(cartItems.length > 0){
-                //process Order
-                cartItems.map(item => {
-                    amount += parseInt(item.product.price) *  parseInt(item.unit);   
-                });
-    
-                const orderId = uuidv4();
-    
-                const order = new OrderModel({
-                    orderId,
-                    customerId,
-                    amount,
-                    txnId,
-                    status: 'received',
-                    items: cartItems
-                })
-    
-                profile.cart = [];
-                
-                order.populate('items.product').execPopulate();
-                const orderResult = await order.save();
-               
-                profile.orders.push(orderResult);
+      if (cart) {
+        let isExist = false;
 
-                await profile.save();
+        let cartItems = cart.items;
 
-                return orderResult;
+        if (cartItems.length > 0) {
+          cartItems.map((item) => {
+            if (item.product._id.toString() === _id.toString()) {
+              if (isRemove) {
+                cartItems.splice(cartItems.indexOf(item), 1);
+              } else {
+                item.unit = qty;
+              }
+              isExist = true;
             }
+          });
         }
 
-      return {}
+        if (!isExist && !isRemove) {
+          cartItems.push({ product: { ...item }, unit: qty });
+        }
 
+        cart.items = cartItems;
+
+        return await cart.save;
+
+      } 
+      else 
+      {
+        return await CartModel.create({
+          customerId,
+          items: [{ product: { ...items }, unit: qty }],
+        });
+      }
+    } catch (err) {
+      throw APIError(
+        "API Error",
+        STATUS_CODES.INTERNAL_ERROR,
+        "Unable to add to Cart"
+      );
     }
+  }
+
+  async CreateNewOrder(customerId, txnId) {
+    //check transaction for payment Status
+    try{
+    const cart = await CartModel.findById(customerId);
+
+    if (cart) {
+      let amount = 0;
+
+      let cartItems = cart.items;
+
+      if (cartItems.length > 0) {
+        //process Order
+        cartItems.map((item) => {
+          amount += parseInt(item.product.price) * parseInt(item.unit);
+        });
+
+        // Here we create a order ID
+        const orderId = uuidv4();
+
+        const order = new OrderModel({
+          orderId,
+          customerId,
+          amount,
+          txnId,
+          status: "received",
+          items: cartItems,
+        });
+
+        cart.items = [];
+
+        const orderResult = await order.save();
+
+        await cart.save();
+
+        return orderResult;
+      }
+    }
+
+    return {};
+
+    }catch(err){
+        throw APIError(
+            "API Error",
+            STATUS_CODES.INTERNAL_ERROR,
+            "Unable to add Order"
+          );
+    }
+  }
 }
 
 module.exports = ShoppingRepository;
